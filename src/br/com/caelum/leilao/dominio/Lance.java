19f4890cb28f706eb0e9688bdd1668f5b52b3331
Gilmar Bernardo da Silva Filho <gilmar.filho@rsinet.com.br>
2019-10-02 13:00:00 -0300

    alteração no lance

--- src/br/com/caelum/leilao/dominio/Lance.java
+++ src/br/com/caelum/leilao/dominio/Lance.java
@@ -4,7 +4,7 @@ public class Lance {
 
 	private Usuario usuario;
 	private double valor;
-	
+
 	public Lance(Usuario usuario, double valor) {
 		this.usuario = usuario;
 		this.valor = valor;
@@ -47,7 +47,5 @@ public class Lance {
 	public double getValor() {
 		return valor;
 	}
-	
-	
-	
+
 }
